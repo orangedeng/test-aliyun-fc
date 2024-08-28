ee5e95f8f9992137726c3f9eb3a4b4fd773e1b7c
Yuxing Deng <jxfa0043379@hotmail.com>
2024-08-28 17:09:30 +0800

    test key

--- main.go
+++ main.go
@@ -12,6 +12,7 @@ type StructEvent struct {
 }
 
 func HandleRequest(ctx context.Context, event StructEvent) (string, error) {
+	fmt.Printf("hello, %s!\n", event.Key)
 	return fmt.Sprintf("hello, %s!", event.Key), nil
 }
 
